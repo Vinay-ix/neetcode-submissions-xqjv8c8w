116e00a499e33e059b3ea226a781cfbe133d0ca4
Vinay <vinaysharma17005@gmail.com>
2026-04-01 15:21:59 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        for(int i:nums){
+            freq[i]++;
+        }
+        priority_queue<pair<int, int>> pq;
+        for (auto it : freq) {
+            pq.push({it.second, it.first});
+        }
+        vector<int> ans;
+        while (k--) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
